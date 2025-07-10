313340f6a7ffa4b7b7916ea58993e62c0339dfcd
Long Deng <ldeng@mail.ustc.edu.cn>
2025-07-10 05:48:12 +0000

    support remapping nodes in convert_to_binary_edgelist

--- tests/convert_to_binary_edgelist.cpp
+++ tests/convert_to_binary_edgelist.cpp
@@ -22,8 +22,10 @@
 #include <string>
 #include <sstream>
 #include <iterator>
+#include <vector>
 #include <boost/shared_ptr.hpp>
 #include <boost/tuple/tuple.hpp>
+#include "cxxopts.hpp"
 
 #include "../webgraph/webgraph.hpp"
 
@@ -32,15 +34,80 @@ int main(int argc, char **argv)
     using namespace std;
     using namespace webgraph::bv_graph;
 
-    if (argc < 3)
-    {
-        cerr << "convert_to_binary_edgelist [webgraph_path] [output_file_path]" << endl;
+    cxxopts::Options options("convert_to_binary_edgelist", "Convert webgraph format to binary edgelist format");
+    
+    options.add_options()
+        ("h,help", "Show help message")
+        ("input", "Input webgraph file path", cxxopts::value<string>())
+        ("output", "Output binary edgelist file path", cxxopts::value<string>())
+        ("do_not_remap", "Disable node ID remapping (even if .nodes file exists)", cxxopts::value<bool>()->implicit_value("true")->default_value("false"))
+        ("short", "Use 32-bit vertex IDs instead of 64-bit", cxxopts::value<bool>()->implicit_value("true")->default_value("false"))
+    ;
+    
+    options.parse_positional({"input", "output"});
+    options.positional_help("<input webgraph file> <output binary edgelist file>");
+    
+    auto result = options.parse(argc, argv);
+    
+    if (result.count("help")) {
+        cout << options.help() << endl;
+        return 0;
+    }
+    
+    if (!result.count("input") || !result.count("output")) {
+        cerr << "Error: Input and output file paths are required" << endl;
+        cout << options.help() << endl;
         return 1;
     }
-
-    string input_name = argv[1];
-    string output_name = argv[2];
-
+    
+    string input_name = result["input"].as<string>();
+    string output_name = result["output"].as<string>();
+    bool use_short = result["short"].as<bool>();
+    
+    if (use_short) {
+        cerr << "Using 32-bit vertex ID format" << endl;
+    } else {
+        cerr << "Using 64-bit vertex ID format" << endl;
+    }
+    
+    // 检查是否存在.nodes文件，如果存在则自动启用重映射
+    string nodes_file = input_name + ".nodes";
+    bool do_not_remap = result["do_not_remap"].as<bool>();
+    bool remap_nodes = false;
+    
+    ifstream nodes_check(nodes_file);
+    if (nodes_check.good()) {
+        nodes_check.close();
+        if (do_not_remap) {
+            cerr << "Detected " << nodes_file << " file, but node remapping is disabled" << endl;
+            remap_nodes = false;
+        } else {
+            cerr << "Detected " << nodes_file << " file, enabling node remapping" << endl;
+            remap_nodes = true;
+        }
+    } else {
+        cerr << "No " << nodes_file << " file detected, node remapping disabled" << endl;
+        remap_nodes = false;
+    }
+    
+    // 读取节点映射文件
+    vector<uint64_t> node_mapping;
+    if (remap_nodes) {
+        ifstream nodes_in(nodes_file);
+        if (!nodes_in.is_open()) {
+            cerr << "Error: Cannot open node mapping file " << nodes_file << endl;
+            return 1;
+        }
+        
+        string line;
+        while (getline(nodes_in, line)) {
+            if (!line.empty()) {
+                node_mapping.push_back(stoull(line));
+            }
+        }
+        nodes_in.close();
+        cerr << "Loaded " << node_mapping.size() << " node mappings" << endl;
+    }
 
     typedef boost::shared_ptr<graph> graph_ptr;
 
@@ -56,6 +123,11 @@ int main(int argc, char **argv)
     cerr << gp->get_num_nodes() << endl;
 
     ofstream fout(output_name, ios::binary);
+    if(!fout.is_open()) {
+        cout << "Open output file failed." << endl;
+        exit(1);
+    }
+
 
     size_t node_count = 0;
     size_t edge_count = 0;
@@ -73,8 +145,26 @@ int main(int argc, char **argv)
             // cerr << edge_count << "th" << endl;
             uint64_t src = *n;
             uint64_t dest = *succ;
-            fout.write((char *)&src, sizeof(uint64_t));
-            fout.write((char *)&dest, sizeof(uint64_t));
+            
+            // 应用节点映射
+            if (remap_nodes) {
+                if (src < node_mapping.size()) {
+                    src = node_mapping[src];
+                }
+                if (dest < node_mapping.size()) {
+                    dest = node_mapping[dest];
+                }
+            }
+            
+            if (use_short) {
+                uint32_t src32 = static_cast<uint32_t>(src);
+                uint32_t dest32 = static_cast<uint32_t>(dest);
+                fout.write((char *)&src32, sizeof(uint32_t));
+                fout.write((char *)&dest32, sizeof(uint32_t));
+            } else {
+                fout.write((char *)&src, sizeof(uint64_t));
+                fout.write((char *)&dest, sizeof(uint64_t));
+            }
             
             // cerr << src << " " << dest << endl;
             
